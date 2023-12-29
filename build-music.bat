1ed66f028e32f92e50e494b5858a5bad0abbde86
Johannes Förstner <johannes@famfoerstner.de>
2023-12-29 17:24:39 +0100

    Define consts for copyright & author

--- build-music.bat
+++ build-music.bat
@@ -1,6 +1,9 @@
+set copyright=CC-BY-SA 2023
+set author=Johannes Förstner
+
 :: Compile music file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_ds%\Bits\world\global\moods\%map%" "%tmp%\Bits\world\global\moods\%map%" /E
 robocopy "%doc_ds%\Bits\sound" %tmp%\Bits\sound /E
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Resources\%map_cs%-music.dsres" -copyright "CC-BY-SA 2023" -title "%map_cs%" -author "Johannes Förstner"
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Resources\%map_cs%-music.dsres" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
 if %errorlevel% neq 0 pause
--- build.bat
+++ build.bat
@@ -14,6 +14,9 @@ set tc=..\TankCreator
 :: path of GasPy
 set gaspy=..\gaspy
 
+set copyright=CC-BY-SA 2023
+set author=Johannes Förstner
+
 :: param
 set mode=%1
 echo %mode%
@@ -39,7 +42,7 @@ pushd %gaspy%
 venv\Scripts\python -m build.fix_start_positions_required_levels %map% "%tmp%\Bits"
 if %errorlevel% neq 0 pause
 popd
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "CC-BY-SA 2023" -title "%map_cs%" -author "Johannes Förstner"
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
 if %errorlevel% neq 0 pause
 
 :: Compile main resource file
@@ -49,13 +52,13 @@ robocopy "%doc_ds%\Bits\world\ai\jobs\%map%" "%tmp%\Bits\world\ai\jobs\%map%" /E
 robocopy "%doc_ds%\Bits\world\contentdb\templates\%map%" "%tmp%\Bits\world\contentdb\templates\%map%" /E
 robocopy "%doc_ds%\Bits\world\global\moods\%map%" "%tmp%\Bits\world\global\moods\%map%" /E
 call "%doc_ds%\Bits\build-music-fallback.bat"
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Resources\%map_cs%.dsres" -copyright "CC-BY-SA 2023" -title "%map_cs%" -author "Johannes Förstner"
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Resources\%map_cs%.dsres" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
 if %errorlevel% neq 0 pause
 
 :: Compile German language resource file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_ds%\Bits\language" "%tmp%\Bits\language" %map%-* /S
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Resources\%map_cs%-de.dsres" -copyright "CC-BY-SA 2023" -title "%map_cs%" -author "Johannes Förstner"
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Resources\%map_cs%-de.dsres" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
 if %errorlevel% neq 0 pause
 
 if not "%mode%"=="light" (
