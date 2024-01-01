562e54a9b3509acb87981ee9035b8abc14cc44f5
Johannes Förstner <johannes@famfoerstner.de>
2024-01-01 15:22:27 +0100

    Fix cleanup after run

--- run.bat
+++ run.bat
@@ -17,4 +17,4 @@ call "%doc_ds%\Bits\build.bat" %*
 "%ds%\DSLOA.exe" nointro=true map=%map%
 
 :: Cleanup resources so as not to confuse Siege Editor
-call "%doc_dsloa%\Bits\cleanup.bat" %*
+call "%doc_ds%\Bits\cleanup.bat" %*
